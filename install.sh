9b2dd96a9506d1ae77e516def43def0c49c34e79
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2023-04-26 12:32:13 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-pacman -Sy
-sudo su
-mkdir .config/
-pacman -S i3
-pacman -S lightdm
-mv i3/config ~/.config/i3/config
-mv i3blocks/config ~/.config/i3blocks/config
-sudo lightdm
+command() {
+  pacman -Sy
+  sudo su
+  mkdir .config/
+  pacman -S i3
+  pacman -S lightdm
+  mv i3/config ~/.config/i3/config
+  mv i3blocks/config ~/.config/i3blocks/config
+  sudo lightdm
+}
