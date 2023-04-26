e5b0117ebf16a2a91a4b7efabdb022b12aa62bde
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2023-04-26 16:34:31 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 commands() {
-  mkdir ~/.config
+  mkdir ~/bg/
+  mkdir ~/.config/
   mkdir ~/.config/i3/
   mkdir ~/.config/i3blocks/
+  
   cp -r i3/config ~/.config/i3/config
-  cp i3blocks/config ~/.config/i3blocks/config
+  cp -r i3blocks/config ~/.config/i3blocks/config
+  cp -r bg/waifu.jpg ~/bg/waifu.jpg
 }
+
+commands
