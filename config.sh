753d67b0be4ea0b232ff987f8eedf4075db76eb0
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2023-04-26 15:21:54 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,6 @@
 commands() {
   mkdir ~/.config
   mkdir ~/.config/i3/
-  mkdir ~/.config/i3blocks/
-  cp i3/config ~/.config/i3/config
+  cp -r i3/ ~/.config/i3/
   cp i3blocks/config ~/.config/i3blocks/config
 }
