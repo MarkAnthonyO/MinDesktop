22be74cae10d1fc56ad866ed5608f81360eb3f83
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2023-04-26 13:20:45 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 command() {
   pacman -Sy
-  mkdir .config/
+  mkdir ~/.config/i3/
+  mkdir ~/.config/i3blocks/
   pacman -S i3
-  pacman -S lightdm
-  mv i3/config ~/.config/i3/config
-  mv i3blocks/config ~/.config/i3blocks/config
-  sudo lightdm
+  pacman -S lightdm-gtk-greeter
+  mv /i3/config ~/.config/i3/config
+  mv /i3blocks/config ~/.config/i3blocks/config
+  systemctl lightdm enable
+  lightdm
 }
 
 command
