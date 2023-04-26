b7ee31be2006675282cc864ae58d49f366257a5d
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2023-04-26 14:57:27 -0600

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+commands() {
+  mkdir ~/.config
+  mkdir ~/.config/i3/
+  mkdir ~/.config/i3blocks/
+  cp /i3/config ~/.config/i3/config
+  cp /i3blocks/config ~/.config/i3blocks/config
+}
