fab4d3d7b5b797726aa35a9addb8ec62f8978619
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2023-05-21 15:15:45 -0600

    0.0.4

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 command() {
+  # Instalation packages
   sudo pacman -Sy
   sudo pacman -S i3
   sudo pacman -S lightdm-gtk-greeter
@@ -12,10 +13,37 @@ command() {
   sudo pacman -S ttf-font-awesome
   sudo pacman -S pulseaudio
   sudo pacman -S pavucontrol
+  sudo pacman -S 
   sudo systemctl enable lightdm
+  
+  # Installation PowerMenu
+  git clone https://github.com/MarkAnthonyO/PowerMenu.git  
+
+  # Installation Paru AUR Helper
+  sudo pacman -S --needed base-devel
+  git clone https://aur.archlinux.org/paru.git
+  
+
+  # Configurations
+  mkdir ~/bg/
+  mkdir ~/.config/
+  mkdir ~/.config/i3/
+  mkdir ~/.config/polybar/
+
+  cp -r i3/config ~/.config/i3/config
+  cp -r polybar/config.ini  ~/.config/polybar/config.ini
+  cp -r bg/bg.jpg ~/bg/bg.jpg
+
   cd ..
-  git clone https://github.com/MarkAnthonyO/PowerMenu.git
-  cd MinDesktop/
+
+  cd PowerMenu/
+  mkdir ~/.config/PowerMenu/
+  cp -r config/ ~/.config/PowerMenu/
+
+  cd ..
+
+  cd paru
+  makepkg -si
 }
 
 command
