4fe898e9ad9db75b7045ca4ca3dcbc1ea664a84c
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2023-05-21 14:54:12 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ command() {
   sudo pacman -S rofi
   sudo pacman -S feh
   sudo pacman -S tk
+  sudo pacman -S noto-fonts
+  sudo pacman -S noto-fonts-emoji
+  sudo pacman -S noto-fonts-cjf
+  sudo pacman -S ttf-font-awesome
+  sudo pacman -S pulseaudio
+  sudo pacman -S pavucontrol
   sudo systemctl enable lightdm
   cd ..
   git clone https://github.com/MarkAnthonyO/PowerMenu.git
