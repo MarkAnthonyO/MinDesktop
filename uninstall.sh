c836c637f024b0c080e60354bc89ae14d1699620
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2023-09-13 18:18:18 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,20 +3,15 @@ command() {
   # Uninstall packages
   sudo pacman -Syu
   sudo pacman -Rsn tk noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-font-awesome otf-font-awesome xdg-desktop-portal-gtk xdg-user-dirs gnome-screenshot sxhkd nitrogen picom polybar dunst sddm python konsole yay
-  rm -r yay/
-  rm -r PowerMenu/
-  rm -r ~/.config/PowerMenu/
+  sudo rm -r yay/
+  sudo rm -r PowerMenu/
+  sudo rm -r ~/.config/PowerMenu/
   # Remove configurations
-  rm -r ~/.config/bspwm/
-  rm -r ~/.config/sxhkd/
-  rm -r ~/.config/polybar/
-  rm -r ~/.config/dunst/
-  rm -r ~/.config/picom.conf
-
-  cd PowerMenu/
-  mkdir ~/.config/PowerMenu/
-  cp -r config/ ~/.config/PowerMenu/
-  cd ..
+  sudo rm -r ~/.config/bspwm/
+  sudo rm -r ~/.config/sxhkd/
+  sudo rm -r ~/.config/polybar/
+  sudo rm -r ~/.config/dunst/
+  sudo rm -r ~/.config/picom.conf
 }
 
 command
