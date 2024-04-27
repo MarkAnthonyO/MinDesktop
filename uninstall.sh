d2eb0f16aaff990609da11346d26003c9a73aad6
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-04-27 00:09:50 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,11 +3,10 @@ command() {
   # Uninstall packages
   sudo pacman -Rsnc - < requirements.txt
   # Remove configurations
-  rm -r ~/.config/bspwm/
-  rm -r ~/.config/sxhkd/
-  rm -r ~/.config/polybar/
-  rm -r ~/.config/dunst/
-  rm -r ~/.config/picom.conf
+  rm -r ~/.config/bspwm
+  rm -r ~/.config/sxhkd
+  rm -r ~/.config/MinD
+  rm -r ~/.config/picom
 }
 
 command
