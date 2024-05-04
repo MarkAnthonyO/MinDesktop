be06ce8b34e4e0ab75a8bb745a04e8a915e97bc8
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-05-04 14:42:19 -0600

    removed theme switcher

--- configFiles/MinD/scripts/ChangeDarkTheme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir ~/.config/polybar
-cp ~/.config/MinD/polybar/dark/config.ini ~/.config/polybar/config.ini
-polybar -r
--- configFiles/MinD/scripts/ChangeLightTheme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir ~/.config/polybar
-cp ~/.config/MinD/polybar/light/config.ini ~/.config/polybar/config.ini
-polybar -r
